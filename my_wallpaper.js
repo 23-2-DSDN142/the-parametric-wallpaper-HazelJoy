414ec166c533266f1584c31a7f6eb8d09c060cc6
Hazel Darney <Hazeldarney@yahoo.co.nz>
2023-07-27 10:24:43 +1200

    test commit
    Testing commit process

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,6 +1,6 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
+let rectWidth  = 10;
+let rectHeight = 10;
 
 
 function setup_wallpaper(pWallpaper) {
@@ -9,15 +9,36 @@ function setup_wallpaper(pWallpaper) {
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
   //Grid settings
-  pWallpaper.grid_settings.cell_width  = 200;
-  pWallpaper.grid_settings.cell_height = 200;
+  pWallpaper.grid_settings.cell_width  = 100;
+  pWallpaper.grid_settings.cell_height = 100;
   pWallpaper.grid_settings.row_offset  = 50;
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(255, 240, 255);
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  rectMode(CENTER);
+  angleMode(DEGREES);
+  for(let j = 0; j<11; j++){
+    for(let i = 0; i<11; i++){
+      push();
+      translate(i*10, j*10);
+      rotate(45)
+      stroke(30);
+      if(i % 2 === 0){
+        noFill();
+        strokeWeight(0.25);
+      }
+      else{
+        fill(60, 60, 60, 100);
+        strokeWeight(0.5);
+      }
+      
+      rect(0 ,0, rectWidth, rectHeight);
+     pop();
+   }
+  }
+  //ellipse(100, 100, 10, 10);
 }
